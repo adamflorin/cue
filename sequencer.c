34980ee6b37ee9c725170e7c3419c1347faaee62
Adam Florin <adamflorin@gmail.com>
2014-01-17 20:58:51 -0800

    Iterate through multiple events, output simultaneous events

--- sequencer.c
+++ sequencer.c
@@ -59,6 +59,8 @@ int C74_EXPORT main(void) {
 
 /**
 * Create external.
+*
+* TODO:OPT: generate 'at' and 'msg' symbols.
 */
 void *sequencer_new(t_symbol *s, long argc, t_atom *argv) {
   t_sequencer *x = (t_sequencer *)object_alloc(s_sequencer_class);
@@ -100,10 +102,15 @@ void sequencer_assist(t_sequencer *x, void *b, long m, long a, char *s) {
 }
 
 /**
-* 'dictionary' input: Given a dictionary in a specific format (passed by reference),
-* queue up next events.
+* 'dictionary' input.
+* 
+* Given a dictionary in a specific format (passed by reference), queue up next event.
 *
-* Assume that dictionary is in specified format, and is not empty.
+* Assume that:
+* - dictionary is not empty
+* - first entry is at key "0"
+* - entry values are dictionaries which include an 'at' key
+*   (transport time in ticks of event)
 *
 * @params
 *   symbol - dictionary name
@@ -121,24 +128,17 @@ void sequencer_dictionary(t_sequencer *x, t_symbol *s) {
   // access 'events' dict
   events = dictobj_findregistered_retain(x->dictionary_name);
 
-  // // TODO: sanity check
-  // if (!events) {
-  //   object_error((t_object*)x, "unable to reference dictionary named %s", x->dictionary_name);
-  //   return;
-  // }
-
   // read first event 'at'
-  // TODO: this may not be at "0" if other events have been deleted (?)
   error = dictionary_getdictionary(events, gensym("0"), (t_object **)&event);
   error = dictionary_getfloat(event, gensym("at"), &at);
-
-  // release dictionary
-  dictobj_release(events);
 
   // schedule event
   atom_setfloat(&at_atom, at);
   time_setvalue(x->d_timeobj, NULL, 1, &at_atom);
   time_schedule(x->d_timeobj, NULL);
+
+  // free memory
+  dictobj_release(events);
 }
 
 /**
@@ -151,7 +151,6 @@ void sequencer_stop(t_sequencer *x) {
 /**
 * Time object callback.
 *
-* TODO: continue scanning dict for other events at same time?
 * TODO: delete event once dispatched?
 * TODO: reschedule time object for time of next event.
 *
@@ -159,20 +158,50 @@ void sequencer_stop(t_sequencer *x) {
 */
 void sequencer_tick(t_sequencer *x) {
   t_dictionary *events;
+  t_symbol **event_keys = NULL;
+  t_symbol *event_key;
+  long num_events = 0;
+  long event_index;
   t_dictionary *event;
-  t_max_err error;
+  double event_at;
+  double last_event_at = -1.0;
   long num_msg_atoms;
   t_atom *msg_atoms;
+  t_max_err error;
 
+  // load events
   events = dictobj_findregistered_retain(x->dictionary_name);
 
-  // read first event 'msg'
-  error = dictionary_getdictionary(events, gensym("0"), (t_object **)&event);
-  error = dictionary_getatoms(event, gensym("msg"), &num_msg_atoms, &msg_atoms);
+  // load event keys
+  error = dictionary_getkeys(events, &num_events, &event_keys);
 
-  // release dictionary
+  // iterate through events
+  for (event_index = 0; event_index < num_events; event_index++) {
+    event_key = event_keys[event_index];
+
+    // load event
+    error = dictionary_getdictionary(events, event_key, (t_object **)&event);
+
+    // Load event 'at', test against last event.
+    // If this event is not at the same time, break.
+    error = dictionary_getfloat(event, gensym("at"), &event_at);
+    if ((last_event_at != -1.0) && (last_event_at != event_at)) {
+      last_event_at = event_at;
+      break;
+    }
+    last_event_at = event_at;
+
+    // load event message
+    error = dictionary_getatoms(event, gensym("msg"), &num_msg_atoms, &msg_atoms);
+
+    // output event message
+    outlet_list(x->d_outlet, 0L, num_msg_atoms, msg_atoms);
+
+    // TODO: delete event once dispatched
+    // error = dictionary_deleteentry (events, event_key);
+  }
+
+  // free memory
+  if (event_keys) dictionary_freekeys(events, num_events, event_keys);
   dictobj_release(events);
-
-  // output msg atoms
-  outlet_list(x->d_outlet, 0L, num_msg_atoms, msg_atoms);
 }
