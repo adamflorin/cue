1789c2ed78a1f1b6de0d1589ba136b4930676452
Adam Florin <adamflorin@gmail.com>
2014-01-18 18:38:36 -0800

    Linked list implementation, matching existing Loom spec

--- sequencer.c
+++ sequencer.c
@@ -14,23 +14,25 @@
 
 // External struct
 // 
-typedef struct sequencer {
-  t_object d_obj;
-  void *d_outlet;
-  t_object *d_timeobj;
-  t_symbol *dictionary_name;
+typedef struct _sequencer {
+  t_object object;
+  void *outlet;
+  t_object *timer;
+  t_linklist  *queue;
   t_bool verbose;
 } t_sequencer;
 
 // Method headers
 // 
-void *sequencer_new(t_symbol *s, long argc, t_atom *argv);
+t_sequencer *sequencer_new(t_symbol *s, long argc, t_atom *argv);
 void sequencer_free(t_sequencer *x);
 void sequencer_assist(t_sequencer *x, void *b, long m, long a, char *s);
+void sequencer_at(t_sequencer *x, t_symbol *msg, long argc, t_atom *argv);
+void sequencer_schedule(t_sequencer *x);
 void sequencer_stop(t_sequencer *x);
-void sequencer_dictionary(t_sequencer *x, t_symbol *s);
-void sequencer_tick(t_sequencer *x);
-void sequencer_schedule(t_sequencer *x, double at_ticks);
+long sequencer_sort_list(void *left, void *right);
+void sequencer_timer_callback(t_sequencer *x);
+void sequencer_schedule_next(t_sequencer *x, double at_ticks);
 
 // External class
 // 
@@ -50,7 +52,8 @@ int C74_EXPORT main(void) {
     0);
 
   class_addmethod(c, (method)sequencer_assist, "assist", A_CANT, 0);
-  class_addmethod(c, (method)sequencer_dictionary, "dictionary", A_SYM, 0);
+  class_addmethod(c, (method)sequencer_at, "at", A_GIMME, 0);
+  class_addmethod(c, (method)sequencer_schedule, "schedule", 0);
   class_addmethod(c, (method)sequencer_stop, "stop", 0);
  
   class_register(CLASS_BOX, c);
@@ -61,21 +64,23 @@ int C74_EXPORT main(void) {
 
 /**
 * Create external.
-*
-* TODO:OPT: generate 'at' and 'msg' symbols.
 */
-void *sequencer_new(t_symbol *s, long argc, t_atom *argv) {
+t_sequencer *sequencer_new(t_symbol *s, long argc, t_atom *argv) {
   t_sequencer *x = (t_sequencer *)object_alloc(s_sequencer_class);
   
   // outlet
-  x->d_outlet = listout(x);
+  x->outlet = listout(x);
   
   // time object (used to schedule events)
-  x->d_timeobj = (t_object*) time_new(
+  x->timer = (t_object*) time_new(
     (t_object *)x,
     gensym("delaytime"),
-    (method)sequencer_tick,
+    (method)sequencer_timer_callback,
     TIME_FLAGS_TICKSONLY | TIME_FLAGS_USECLOCK);
+
+  // queue
+  x->queue = linklist_new();
+  // linklist_flags(x->queue, OBJ_FLAG_OBJ);
 
   // debug mode
   x->verbose = true;
@@ -87,7 +92,8 @@ void *sequencer_new(t_symbol *s, long argc, t_atom *argv) {
 * Destroy external.
 */
 void sequencer_free(t_sequencer *x) {
-  freeobject(x->d_timeobj);
+  freeobject(x->timer);
+  object_free(x->queue);
 }
 
 /**
@@ -96,120 +102,124 @@ void sequencer_free(t_sequencer *x) {
 void sequencer_assist(t_sequencer *x, void *b, long m, long a, char *s) {
   if (m == ASSIST_INLET) {
     switch (a) {
-      case 0: sprintf(s, "Sequence bang"); break;
+      case 0: sprintf(s, "Events, schedule, stop"); break;
     }
   }
   else {
     switch (a) {
-      case 0: sprintf(s, "Bang at time"); break;
+      case 0: sprintf(s, "Event dispatch"); break;
     }
   }
 }
 
 /**
-* 'dictionary' input.
-* 
-* Given a dictionary in a specific format (passed by reference), queue up next event.
+* 'at' input: Insert received event into queue at appropriate place.
 *
-* Assume that:
-* - dictionary is not empty
-* - first entry is at key "0"
-* - entry values are dictionaries which include an 'at' key
-*   (transport time in ticks of event)
-*
-* @params
-*   symbol - dictionary name
+* Don't re-sort here, as events are generally added in batches.
+* See 'schedule' message.
 */
-void sequencer_dictionary(t_sequencer *x, t_symbol *s) {
-  t_dictionary *events;
-  long num_events = 0;
-  t_symbol **event_keys = NULL;
-  t_dictionary *event;
+void sequencer_at(t_sequencer *x, t_symbol *msg, long argc, t_atom *argv) {
   t_max_err error;
-  double at_ticks;
+  t_atomarray *event_atoms;
+  t_atom_long queue_length;
 
-  // store dict name immediately
-  x->dictionary_name = s;
-
-  // access 'events' dict
-  events = dictobj_findregistered_retain(x->dictionary_name);
-
-  // sanity check
-  if (!events) {
-    object_error((t_object*)x, "Error loading '%s'", x->dictionary_name->s_name);
-    return;
-  }
-
-  // load event keys
-  error = dictionary_getkeys(events, &num_events, &event_keys);
+  // Build event from copy of args (atom array)
+  event_atoms = atomarray_new(0, 0L);
+  error = atomarray_setatoms(event_atoms, argc, argv);
   if (error) {
-    object_error((t_object *)x, "Error loading key from '%s'. (%d)", x->dictionary_name->s_name, error);
+    object_error((t_object *)x, "Error copying event message. (%d)", error);
+    return;
+  }
+  
+  // push event onto queue
+  queue_length = linklist_insert_sorted(x->queue, event_atoms, sequencer_sort_list);
+  if (queue_length == -1) {
+    object_error((t_object *)x, "Error adding event to queue.");
     return;
   }
 
-  // if (x->verbose) dictionary_dump(events, 1, 0);
-
-  if (num_events > 0) {
-    // dictionary is not empty
-    if (x->verbose) object_post((t_object*)x, "First key in received dict %s is '%s'.", x->dictionary_name->s_name, event_keys[0]->s_name);
-
-    // read first event 'at'
-    error = dictionary_getdictionary(events, event_keys[0], (t_object **)&event);
-    if (error) {
-      object_error((t_object *)x, "Error loading event from '%s'. (%d)", x->dictionary_name->s_name, error);
-      return;
-    }
-
-    error = dictionary_getfloat(event, gensym("at"), &at_ticks);
-    if (error) {
-      object_error((t_object *)x, "Error loading event time from '%s'. (%d)", x->dictionary_name->s_name, error);
-      return;
-    }
-
-    // schedule event
-    sequencer_schedule(x, at_ticks);
-  } else {
-    // dictionary is empty
-    sequencer_stop(x);
-  }
-
-  // release dict
-  error = dictobj_release(events);
-  if (error) {
-    object_error((t_object *)x, "Error releasing dict from '%s'. (%d)", x->dictionary_name->s_name, error);
-    return;
-  }
+  if (x->verbose) object_post((t_object*)x, "Added event. Queue size %d.", queue_length);
 }
 
 /**
-* 'stop' input: shut it down.
-*
-* TODO: clear Dict, too? Or will [js] be responsible for that?
+* 'schedule' input: Set timer to fire for first event in queue.
+* 
+* Assume queue is sorted.
+*/
+void sequencer_schedule(t_sequencer *x) {
+  t_max_err error;
+  t_atomarray *event_atoms;
+  t_atom *out_atoms;
+  long num_out_atoms;
+  t_atom_float event_at; // float? double?
+  
+  // Pull first item off of queue
+  event_atoms = (t_atomarray *)linklist_getindex(x->queue, 0);
+  if (event_atoms == NULL) {
+    object_error((t_object *)x, "No event to schedule.");
+    return;
+  }
+
+  // copy event atoms
+  // TODO: is atomarray_getatoms() safe?
+  error = atomarray_copyatoms(event_atoms, &num_out_atoms, &out_atoms);
+  if (error) {
+    object_error((t_object *)x, "Error copying event message for scheduling. (%d)", error);
+    return;
+  }
+
+  // get next event time.
+  event_at = atom_getfloat(out_atoms);
+
+  // set timer
+  sequencer_schedule_next(x, event_at);
+}
+
+/**
+* 'stop' input: Stop timer, clear linked list.
 */
 void sequencer_stop(t_sequencer *x) {
-  time_stop(x->d_timeobj);
+  time_stop(x->timer);
+  linklist_clear(x->queue);
 }
 
 /**
-* Time object callback.
-*
-* TODO: reset or anything?
-*
-* XTRA:TODO: sanity check on dict format
+* Given two events (atom arrays), determine which is greater,
+* based on first value (i.e. "at" time in ticks).
 */
-void sequencer_tick(t_sequencer *x) {
-  t_dictionary *events;
-  t_symbol **event_keys = NULL;
-  t_symbol *event_key;
-  char *event_key_string;
-  long num_events = 0;
-  long event_index;
-  t_dictionary *event;
-  double event_at;
-  double last_event_at = -1.0;
-  long num_msg_atoms;
-  t_atom *msg_atoms;
+long sequencer_sort_list(void *left, void *right) {
+  t_atomarray *left_event = (t_atomarray *)left;
+  t_atomarray *right_event = (t_atomarray *)right;
+  long left_event_length = 0;
+  long right_event_length = 0;
+  t_atom *left_event_message;
+  t_atom *right_event_message;
+  t_atom_float left_at;
+  t_atom_float right_at;
   t_max_err error;
+
+  error = atomarray_getatoms(left_event, &left_event_length, &left_event_message);
+  error = atomarray_getatoms(right_event, &right_event_length, &right_event_message);
+
+  left_at = atom_getfloat(left_event_message);
+  right_at = atom_getfloat(right_event_message);
+
+  return left_at < right_at;
+}
+
+/**
+* Time object callback: Output all events in queue specified to fire at this time.
+*
+* If any future events remain in the queue, re-schedule them to fire at their time.
+*/
+void sequencer_timer_callback(t_sequencer *x) {
+  t_atomarray *event_atoms;
+  t_max_err error;
+  t_atom *out_atoms;
+  long num_out_atoms;
+  t_atom_long deleted_index;
+  t_atom_float event_at; // float? double?
+  double last_event_at = -1.0;
 
   // get current time
   // itm = (t_itm *)itm_getglobal();
@@ -218,76 +228,52 @@ void sequencer_tick(t_sequencer *x) {
   // return if transport is stopped
   // if (itm_getstate(itm) == 0) return;
 
-  // load events
-  events = dictobj_findregistered_retain(x->dictionary_name);
+  if (x->verbose) object_post((t_object*)x, "Fire event!");
 
-  // sanity check
-  if (!events) {
-    object_error((t_object*)x, "Error loading '%s'", x->dictionary_name->s_name);
-    return;
-  }
+  while (linklist_getsize(x->queue) > 0) {
 
-  // load event keys
-  error = dictionary_getkeys(events, &num_events, &event_keys);
-  if (error) {
-    object_error((t_object *)x, "Error loading keys from '%s'. (%d)", x->dictionary_name->s_name, error);
-    return;
-  }
+    if (x->verbose) object_post((t_object*)x, "Pop event to fire.");
 
-  // iterate through events
-  for (event_index = 0; event_index < num_events; event_index++) {
-    event_key = event_keys[event_index];
-
-    // For NO apparent reason, THIS line makes everything work
-    event_key_string = event_key->s_name;
-
-    // load event
-    error = dictionary_getdictionary(events, event_key, (t_object **)&event);
-    if (error) {
-      object_error((t_object *)x, "Error loading event from '%s'. (%d)", x->dictionary_name->s_name, error);
+    // Pull first item off of queue
+    event_atoms = (t_atomarray *)linklist_getindex(x->queue, 0);
+    if (event_atoms == NULL) {
+      object_error((t_object *)x, "No first event in queue.");
       break;
     }
 
-    // Load event time ('at')
-    error = dictionary_getfloat(event, gensym("at"), &event_at);
+    // copy event atoms
+    // TODO: is atomarray_getatoms() safe?
+    error = atomarray_copyatoms(event_atoms, &num_out_atoms, &out_atoms);
     if (error) {
-      object_error((t_object *)x, "Error loading event time from '%s'. (%d)", x->dictionary_name->s_name, error);
-      return;
+      object_error((t_object *)x, "Error copying event message for output. (%d)", error);
+      break;
     }
 
-    // Test even time against last event.
+    // Test event time against last event.
     // If this event is not at the same time, break,
     // and reschedule time object to fire at event time.
+    event_at = atom_getfloat(out_atoms);
+
+    if (x->verbose) object_post((t_object*)x, "Event to fire claims to be at %f ticks.", event_at);
+
     if ((last_event_at != -1.0) && (last_event_at != event_at)) {
-      sequencer_schedule(x, event_at);
+      sequencer_schedule_next(x, event_at);
       break;
     }
     last_event_at = event_at;
 
-    // load event message
-    error = dictionary_getatoms(event, gensym("msg"), &num_msg_atoms, &msg_atoms);
-    if (error) {
-      object_error((t_object *)x, "Error loading event message from '%s'. (%d)", x->dictionary_name->s_name, error);
+    // output event message (minus first atom)
+    // TODO: make sure message is longer than one atom?
+    outlet_list(x->outlet, 0L, num_out_atoms-1, out_atoms+1);
+
+    // remove event from queue
+    deleted_index = linklist_deleteindex(x->queue, 0);
+    if (deleted_index == -1) {
+      object_error((t_object *)x, "Error deleting first event in queue.");
       return;
     }
 
-    // output event message
-    outlet_list(x->d_outlet, 0L, num_msg_atoms, msg_atoms);
-
-    // delete event
-    error = dictionary_deleteentry(events, event_key);
-    if (error) {
-      object_error((t_object *)x, "Error deleting event from '%s'. (%d)", x->dictionary_name->s_name, error);
-      break;
-    }
-  }
-
-  // free memory
-  if (event_keys) dictionary_freekeys(events, num_events, event_keys);
-  error = dictobj_release(events);
-  if (error) {
-    object_error((t_object *)x, "Error releasing events from '%s'. (%d)", x->dictionary_name->s_name, error);
-    return;
+    if (x->verbose) object_post((t_object*)x, "Deleted first event from queue. Loop!");
   }
 }
 
@@ -296,7 +282,7 @@ void sequencer_tick(t_sequencer *x) {
 *
 * This method is used internally--not exposed as an input.
 */
-void sequencer_schedule(t_sequencer *x, double at_ticks) {
+void sequencer_schedule_next(t_sequencer *x, double at_ticks) {
   t_itm *itm;
   double now_ticks;
   t_atom next_event_at_atom;
@@ -306,15 +292,15 @@ void sequencer_schedule(t_sequencer *x, double at_ticks) {
   itm = (t_itm *)itm_getglobal();
   now_ticks = itm_getticks(itm);
 
-  if (x->verbose) object_post((t_object*)x, "Scheduling %s for %f (now: %f).", x->dictionary_name->s_name, at_ticks, now_ticks);
+  if (x->verbose) object_post((t_object*)x, "Attempting to schedule timer at %f (now: %f).", at_ticks, now_ticks);
 
   if (at_ticks >= now_ticks) {
     error = atom_setfloat(&next_event_at_atom, at_ticks - now_ticks);
     if (error) {
-      object_error((t_object *)x, "Error scheduling timer for '%s'. (%d)", x->dictionary_name->s_name, error);
+      object_error((t_object *)x, "Error scheduling timer. (%d)", error);
       return;
     }
-    time_setvalue(x->d_timeobj, NULL, 1, &next_event_at_atom);
-    time_schedule(x->d_timeobj, NULL);
+    time_setvalue(x->timer, NULL, 1, &next_event_at_atom);
+    time_schedule(x->timer, NULL);
   }
 }
