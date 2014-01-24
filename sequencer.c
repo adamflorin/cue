060ec04fdb568715c3de540313215b5490a84508
Adam Florin <adamflorin@gmail.com>
2014-01-23 18:55:42 -0800

    Refactor into sequencer_iterate; reject events outside of grace period; accept 'name' for debugging

--- sequencer.c
+++ sequencer.c
@@ -20,6 +20,7 @@ typedef struct _sequencer {
   void *done_outlet;
   t_object *timer;
   t_linklist  *queue;
+  t_symbol *name;
   t_bool verbose;
   t_bool overrideNow;
 } t_sequencer;
@@ -29,12 +30,16 @@ typedef struct _sequencer {
 t_sequencer *sequencer_new(t_symbol *s, long argc, t_atom *argv);
 void sequencer_free(t_sequencer *x);
 void sequencer_assist(t_sequencer *x, void *b, long m, long a, char *s);
+void sequencer_name(t_sequencer *x, t_symbol *name);
 void sequencer_at(t_sequencer *x, t_symbol *msg, long argc, t_atom *argv);
 void sequencer_schedule(t_sequencer *x);
 void sequencer_stop(t_sequencer *x);
 long sequencer_sort_list(void *left, void *right);
 void sequencer_timer_callback(t_sequencer *x);
+void sequencer_iterate(t_sequencer *x, t_bool output_now);
 void sequencer_schedule_next(t_sequencer *x, double at_ticks);
+
+const double GRACE_PERIOD_TICKS = 1.0;
 
 // External class
 // 
@@ -54,6 +59,7 @@ int C74_EXPORT main(void) {
     0);
 
   class_addmethod(c, (method)sequencer_assist, "assist", A_CANT, 0);
+  class_addmethod(c, (method)sequencer_name, "name", A_SYM, 0);
   class_addmethod(c, (method)sequencer_at, "at", A_GIMME, 0);
   class_addmethod(c, (method)sequencer_schedule, "schedule", 0);
   class_addmethod(c, (method)sequencer_stop, "stop", 0);
@@ -85,8 +91,11 @@ t_sequencer *sequencer_new(t_symbol *s, long argc, t_atom *argv) {
   // queue
   x->queue = linklist_new();
 
+  // name
+  x->name = NULL;
+
   // debug mode
-  x->verbose = false;
+  x->verbose = true;
 
   // set "now" override--if transport is stopped
   itm = (t_itm *)time_getitm(x->timer);
@@ -120,6 +129,14 @@ void sequencer_assist(t_sequencer *x, void *b, long m, long a, char *s) {
 }
 
 /**
+* Name instance for debugging purposes.
+*/
+void sequencer_name(t_sequencer *x, t_symbol *name) {
+  if (x->verbose) object_post((t_object*)x, "Naming %s.", name->s_name);
+  x->name = name;
+}
+
+/**
 * 'at' input: Insert received event into queue at appropriate place.
 *
 * Don't re-sort here, as events are generally added in batches.
@@ -134,18 +151,18 @@ void sequencer_at(t_sequencer *x, t_symbol *msg, long argc, t_atom *argv) {
   event_atoms = atomarray_new(0, 0L);
   error = atomarray_setatoms(event_atoms, argc, argv);
   if (error) {
-    object_error((t_object *)x, "Error copying event message. (%d)", error);
+    object_error((t_object *)x, "Error copying event message for %s. (%d)", x->name->s_name, error);
     return;
   }
   
   // push event onto queue
   queue_length = linklist_insert_sorted(x->queue, event_atoms, sequencer_sort_list);
   if (queue_length == -1) {
-    object_error((t_object *)x, "Error adding event to queue.");
+    object_error((t_object *)x, "Error adding event to queue for %s.", x->name->s_name);
     return;
   }
 
-  if (x->verbose) object_post((t_object*)x, "Added event. Queue size %d.", queue_length);
+  if (x->verbose) object_post((t_object*)x, "Added event to %s. Queue size %d.", x->name->s_name, queue_length);
 }
 
 /**
@@ -154,30 +171,7 @@ void sequencer_at(t_sequencer *x, t_symbol *msg, long argc, t_atom *argv) {
 * Assume queue is sorted.
 */
 void sequencer_schedule(t_sequencer *x) {
-  t_max_err error;
-  t_atomarray *event_atoms;
-  t_atom *out_atoms;
-  long num_out_atoms;
-  t_atom_float event_at; // float? double?
-  
-  // pull first item off of queue
-  event_atoms = (t_atomarray *)linklist_getindex(x->queue, 0);
-
-  // if queue is empty, bail
-  if (event_atoms == NULL) return;
-
-  // copy event atoms
-  error = atomarray_getatoms(event_atoms, &num_out_atoms, &out_atoms);
-  if (error) {
-    object_error((t_object *)x, "Error copying event message for scheduling. (%d)", error);
-    return;
-  }
-
-  // get next event time.
-  event_at = atom_getfloat(out_atoms);
-
-  // set timer
-  sequencer_schedule_next(x, event_at);
+  sequencer_iterate(x, false);
 }
 
 /**
@@ -219,67 +213,102 @@ long sequencer_sort_list(void *left, void *right) {
 * If any future events remain in the queue, re-schedule them to fire at their time.
 */
 void sequencer_timer_callback(t_sequencer *x) {
-  t_atomarray *event_atoms;
+  // if (x->verbose) object_post((t_object*)x, "Callback fired in %s", x->name->s_name);
+
+  sequencer_iterate(x, true);
+}
+
+
+/**
+* Iterate through queue, scheduling next events, deleting past events, and outputting current ones.
+*
+* @param output_now - if true, output events. If false, just schedule first event.
+*/
+void sequencer_iterate(t_sequencer *x, t_bool output_now) {
   t_max_err error;
+  t_itm *itm;
+  double now_ticks;
+  t_atomarray *event_atoms;
   t_atom *out_atoms;
   long num_out_atoms;
+  t_atom_float event_at;
+  t_atom_float event_from_now;
   t_atom_long deleted_index;
-  t_atom_float event_at; // float? double?
   double last_event_at = -1.0;
 
-  if (x->verbose) object_post((t_object*)x, "Fire event!");
+  // get current time
+  itm = (t_itm *)time_getitm(x->timer);
+  now_ticks = x->overrideNow ? 0.0 : itm_getticks(itm);
 
+  // iterate through events
   while (linklist_getsize(x->queue) > 0) {
-
-    if (x->verbose) object_post((t_object*)x, "Pop event to fire.");
 
     // Pull first item off of queue
     event_atoms = (t_atomarray *)linklist_getindex(x->queue, 0);
     if (event_atoms == NULL) {
-      object_error((t_object *)x, "No first event in queue.");
-      break;
+      object_error((t_object *)x, "No first event in queue for %s.", x->name->s_name);
+      return;
     }
 
     // copy event atoms
     error = atomarray_getatoms(event_atoms, &num_out_atoms, &out_atoms);
     if (error) {
-      object_error((t_object *)x, "Error copying event message for output. (%d)", error);
-      break;
-    }
-
-    // Test event time against last event.
-    // If this event is not at the same time, break,
-    // and reschedule time object to fire at event time.
-    event_at = atom_getfloat(out_atoms);
-
-    if (x->verbose) object_post((t_object*)x, "Event to fire claims to be at %f ticks.", event_at);
-
-    if ((last_event_at != -1.0) && (last_event_at != event_at)) {
-      sequencer_schedule_next(x, event_at);
-      break;
-    }
-    last_event_at = event_at;
-
-    // output event message (minus first atom)
-    if (num_out_atoms > 1) {
-      outlet_list(x->event_outlet, 0L, num_out_atoms-1, out_atoms+1);
-    }
-
-    // remove event from queue
-    deleted_index = linklist_deleteindex(x->queue, 0);
-    if (deleted_index == -1) {
-      object_error((t_object *)x, "Error deleting first event in queue.");
+      object_error((t_object *)x, "Error copying event message for output for %s. (%d)", x->name->s_name, error);
       return;
     }
 
-    if (x->verbose) object_post((t_object*)x, "Deleted first event from queue. Loop!");
-  }
+    // get event time
+    event_at = atom_getfloat(out_atoms);
+    event_from_now = event_at - now_ticks;
 
-  if (x->verbose) object_post((t_object*)x, "Exited loop. Queue length is %d.", linklist_getsize(x->queue));
+    // if event is indeed in the future
+    if (event_at + GRACE_PERIOD_TICKS >= now_ticks) {
+
+      if (x->verbose) object_post((t_object*)x, "Event from %s claims to be at %f ticks.", x->name->s_name, event_at);
+
+      if (output_now) {
+        // output this event, or schedule next
+
+        // Test event time against last event.
+        // If this event is not at the same time, break,
+        // and reschedule time object to fire at event time.
+        if ((last_event_at != -1.0) && (last_event_at != event_at)) {
+          sequencer_schedule_next(x, event_from_now);
+          break;
+        }
+
+        // output event message (minus first atom)
+        if (num_out_atoms > 1) {
+          outlet_list(x->event_outlet, 0L, num_out_atoms-1, out_atoms+1);
+        }
+
+        // remove event from queue
+        deleted_index = linklist_deleteindex(x->queue, 0);
+        if (deleted_index == -1) {
+          object_error((t_object *)x, "Error deleting first event in queue for %s.", x->name->s_name);
+          return;
+        }
+      } else {
+        // just schedule next event
+        sequencer_schedule_next(x, event_from_now);
+        break;
+      }
+      last_event_at = event_at;
+    } else {
+      // event was in the past: remove from queue and keep iterating
+      object_warn((t_object *)x, "MISSED EVENT FOR %s. Was supposed to be %f, now is %f", x->name->s_name, event_at, now_ticks);
+
+      deleted_index = linklist_deleteindex(x->queue, 0);
+      if (deleted_index == -1) {
+        object_error((t_object *)x, "Error deleting first event in queue for %s.", x->name->s_name);
+        return;
+      }
+    }
+  }
 
   // if queue is now empty, send bang out "done" outlet
   if (linklist_getsize(x->queue) == 0) {
-    if (x->verbose) object_post((t_object*)x, "Queue empty. Send done event.");
+    // if (x->verbose) object_post((t_object*)x, "Queue empty for %s. Send done event.", x->name->s_name);
     outlet_bang(x->done_outlet);
   }
 }
@@ -290,30 +319,23 @@ void sequencer_timer_callback(t_sequencer *x) {
 * This method is used internally--not exposed as an input.
 */
 void sequencer_schedule_next(t_sequencer *x, double at_ticks) {
-  t_itm *itm;
-  double now_ticks;
   t_atom next_event_at_atom;
   t_max_err error;
 
-  // get current time
-  itm = (t_itm *)time_getitm(x->timer);
-  now_ticks = x->overrideNow ? 0.0 : itm_getticks(itm);
-
   if (x->verbose) {
-    object_post((t_object*)x, "Attempting to schedule timer at %f (now: %f).",
-      at_ticks, now_ticks);
+    object_post((t_object*)x, "Attempting to schedule timer for %s at %f.",
+      x->name->s_name,
+      at_ticks);
   }
 
-  if (at_ticks >= now_ticks) {
-    error = atom_setfloat(&next_event_at_atom, at_ticks - now_ticks);
-    if (error) {
-      object_error((t_object *)x, "Error scheduling timer. (%d)", error);
-      return;
-    }
-    time_setvalue(x->timer, NULL, 1, &next_event_at_atom);
-    time_schedule(x->timer, NULL);
-
-    // switch off override
-    x->overrideNow = false;
+  error = atom_setfloat(&next_event_at_atom, at_ticks);
+  if (error) {
+    object_error((t_object *)x, "Error scheduling timer for %s. (%d)", x->name->s_name, error);
+    return;
   }
+  time_setvalue(x->timer, NULL, 1, &next_event_at_atom);
+  time_schedule(x->timer, NULL);
+
+  // switch off override
+  x->overrideNow = false;
 }
