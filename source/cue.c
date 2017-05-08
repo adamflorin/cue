34eb206a20693b21d4bfa4e49c44ff6ebf0d2f17
Adam Florin <hey@adamflorin.work>
2017-05-08 15:58:53 -0700

    Rename project to [cue], update Mac build

--- source/cue.c
+++ source/cue.c
@@ -1,5 +1,5 @@
 /**
-* sequencer.c: Max external to sequence messages at specific transport times.
+* cue.c: Cue Max messages at specified transport times
 *
 * Copyright 2014 Adam Florin
 */
@@ -13,8 +13,8 @@
 #include "ext_dictobj.h"
 
 // External struct
-// 
-typedef struct _sequencer {
+//
+typedef struct _cue {
   t_object object;
   void *event_outlet;
   void *done_outlet;
@@ -26,72 +26,72 @@ typedef struct _sequencer {
   t_symbol *normal_event_msg;
   t_bool verbose;
   t_bool overrideNow;
-} t_sequencer;
+} t_cue;
 
 // Method headers
-// 
-t_sequencer *sequencer_new(t_symbol *s, long argc, t_atom *argv);
-void sequencer_free(t_sequencer *x);
-void sequencer_assist(t_sequencer *x, void *b, long m, long a, char *s);
-void sequencer_name(t_sequencer *x, t_symbol *name);
-void sequencer_at(t_sequencer *x, t_symbol *msg, long argc, t_atom *argv);
-void sequencer_schedule(t_sequencer *x);
-void sequencer_stop(t_sequencer *x);
-long sequencer_sort_list(void *left, void *right);
-void sequencer_timer_callback(t_sequencer *x);
-void sequencer_iterate(t_sequencer *x, t_bool output_now);
-void sequencer_schedule_next(t_sequencer *x, double at_ticks);
+//
+t_cue *cue_new(t_symbol *s, long argc, t_atom *argv);
+void cue_free(t_cue *x);
+void cue_assist(t_cue *x, void *b, long m, long a, char *s);
+void cue_name(t_cue *x, t_symbol *name);
+void cue_at(t_cue *x, t_symbol *msg, long argc, t_atom *argv);
+void cue_schedule(t_cue *x);
+void cue_stop(t_cue *x);
+long cue_sort_list(void *left, void *right);
+void cue_timer_callback(t_cue *x);
+void cue_iterate(t_cue *x, t_bool output_now);
+void cue_schedule_next(t_cue *x, double at_ticks);
 
 // Grace period constants
-// 
+//
 const double IMPORTANT_GRACE_PERIOD_TICKS = 5.0;
 const double NORMAL_GRACE_PERIOD_TICKS = 50.0;
 
 // External class
-// 
-static t_class *s_sequencer_class = NULL;
+//
+static t_class *s_cue_class = NULL;
 
 /**
 * main: proto-init.
 */
 int C74_EXPORT main(void) {
   t_class *c = class_new(
-    "sequencer",
-    (method)sequencer_new,
-    (method)sequencer_free,
-    sizeof(t_sequencer),
+    "cue",
+    (method)cue_new,
+    (method)cue_free,
+    sizeof(t_cue),
     (method)0L,
     A_GIMME,
     0);
 
-  class_addmethod(c, (method)sequencer_assist, "assist", A_CANT, 0);
-  class_addmethod(c, (method)sequencer_name, "name", A_SYM, 0);
-  class_addmethod(c, (method)sequencer_at, "at", A_GIMME, 0);
-  class_addmethod(c, (method)sequencer_schedule, "schedule", 0);
-  class_addmethod(c, (method)sequencer_stop, "stop", 0);
- 
+  class_addmethod(c, (method)cue_assist, "assist", A_CANT, 0);
+  class_addmethod(c, (method)cue_name, "name", A_SYM, 0);
+  class_addmethod(c, (method)cue_at, "at", A_GIMME, 0);
+  class_addmethod(c, (method)cue_schedule, "schedule", 0);
+  class_addmethod(c, (method)cue_stop, "stop", 0);
+
   class_register(CLASS_BOX, c);
 
-  s_sequencer_class = c;
+  s_cue_class = c;
   return 0;
 }
 
 /**
 * Create external.
 */
-t_sequencer *sequencer_new(t_symbol *s, long argc, t_atom *argv) {
-  t_sequencer *x = (t_sequencer *)object_alloc(s_sequencer_class);
+t_cue *cue_new(t_symbol *s, long argc, t_atom *argv) {
+  t_cue *x = (t_cue *)object_alloc(s_cue_class);
   t_itm *itm;
-  
+
   // outlet
   x->done_outlet = bangout(x);
   x->event_outlet = listout(x);
-  
+
   // time object (used to schedule events)
   x->timer = (t_object*) time_new(
     (t_object *)x,
     gensym("delaytime"),
-    (method)sequencer_timer_callback,
+    (method)cue_timer_callback,
     TIME_FLAGS_TICKSONLY | TIME_FLAGS_USECLOCK);
 
   // queue
@@ -118,7 +118,7 @@ t_sequencer *sequencer_new(t_symbol *s, long argc, t_atom *argv) {
 /**
 * Destroy external.
 */
-void sequencer_free(t_sequencer *x) {
+void cue_free(t_cue *x) {
   freeobject(x->timer);
   object_free(x->queue);
 }
@@ -126,7 +126,7 @@ void sequencer_free(t_sequencer *x) {
 /**
 * Hardcode user tooltip prompts.
 */
-void sequencer_assist(t_sequencer *x, void *b, long m, long a, char *s) {
+void cue_assist(t_cue *x, void *b, long m, long a, char *s) {
   if (m == ASSIST_INLET) {
     switch (a) {
       case 0: sprintf(s, "Events, schedule, stop"); break;
@@ -142,7 +142,7 @@ void sequencer_assist(t_sequencer *x, void *b, long m, long a, char *s) {
 /**
 * Name instance for debugging purposes.
 */
-void sequencer_name(t_sequencer *x, t_symbol *name) {
+void cue_name(t_cue *x, t_symbol *name) {
   if (x->verbose) object_post((t_object*)x, "Naming %s.", name->s_name);
   x->name = name;
 }
@@ -150,7 +150,7 @@ void sequencer_name(t_sequencer *x, t_symbol *name) {
 /**
 * 'at' input: Insert received event into queue at appropriate place.
 */
-void sequencer_at(t_sequencer *x, t_symbol *msg, long argc, t_atom *argv) {
+void cue_at(t_cue *x, t_symbol *msg, long argc, t_atom *argv) {
   t_max_err error;
   t_atomarray *event_atoms;
   t_atom_long queue_length;
@@ -162,9 +162,9 @@ void sequencer_at(t_sequencer *x, t_symbol *msg, long argc, t_atom *argv) {
     object_error((t_object *)x, "Error copying event message for %s. (%d)", x->name->s_name, error);
     return;
   }
-  
+
   // push event onto queue
-  queue_length = linklist_insert_sorted(x->queue, event_atoms, sequencer_sort_list);
+  queue_length = linklist_insert_sorted(x->queue, event_atoms, cue_sort_list);
   if (queue_length == -1) {
     object_error((t_object *)x, "Error adding event to queue for %s.", x->name->s_name);
     return;
@@ -175,17 +175,17 @@ void sequencer_at(t_sequencer *x, t_symbol *msg, long argc, t_atom *argv) {
 
 /**
 * 'schedule' input: Set timer to fire for first event in queue.
-* 
+*
 * Assume queue is sorted.
 */
-void sequencer_schedule(t_sequencer *x) {
-  sequencer_iterate(x, false);
+void cue_schedule(t_cue *x) {
+  cue_iterate(x, false);
 }
 
 /**
 * 'stop' input: Stop timer, clear linked list.
 */
-void sequencer_stop(t_sequencer *x) {
+void cue_stop(t_cue *x) {
   time_stop(x->timer);
   linklist_clear(x->queue);
   x->overrideNow = true;
@@ -195,7 +195,7 @@ void sequencer_stop(t_sequencer *x) {
 * Given two events (atom arrays), determine which is greater,
 * based on first value (i.e. "at" time in ticks).
 */
-long sequencer_sort_list(void *left, void *right) {
+long cue_sort_list(void *left, void *right) {
   t_atomarray *left_event = (t_atomarray *)left;
   t_atomarray *right_event = (t_atomarray *)right;
   long left_event_length = 0;
@@ -220,10 +220,10 @@ long sequencer_sort_list(void *left, void *right) {
 *
 * If any future events remain in the queue, re-schedule them to fire at their time.
 */
-void sequencer_timer_callback(t_sequencer *x) {
+void cue_timer_callback(t_cue *x) {
   // if (x->verbose) object_post((t_object*)x, "Callback fired in %s", x->name->s_name);
 
-  sequencer_iterate(x, true);
+  cue_iterate(x, true);
 }
 
 
@@ -232,7 +232,7 @@ void sequencer_timer_callback(t_sequencer *x) {
 *
 * @param output_now - if true, output events. If false, just schedule first event.
 */
-void sequencer_iterate(t_sequencer *x, t_bool output_now) {
+void cue_iterate(t_cue *x, t_bool output_now) {
   t_max_err error;
   t_itm *itm;
   double now_ticks;
@@ -315,7 +315,7 @@ void sequencer_iterate(t_sequencer *x, t_bool output_now) {
       last_event_at = event_at;
     } else {
       // schdule event
-      sequencer_schedule_next(x, event_from_now);
+      cue_schedule_next(x, event_from_now);
       break;
     }
   }
@@ -332,7 +332,7 @@ void sequencer_iterate(t_sequencer *x, t_bool output_now) {
 *
 * This method is used internally--not exposed as an input.
 */
-void sequencer_schedule_next(t_sequencer *x, double at_ticks) {
+void cue_schedule_next(t_cue *x, double at_ticks) {
   t_atom next_event_at_atom;
   t_max_err error;
 
